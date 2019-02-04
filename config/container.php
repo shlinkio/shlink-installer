3de3b8cdf8b62721793ce35768454114838524f5
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-02-04 20:01:50 +0100

    Updated autoloading strategy to work after installing with composer

--- config/container.php
+++ config/container.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 namespace Shlinkio\Shlink\Installer;
 
 use Zend\ServiceManager\ServiceManager;
+use function file_exists;
 
-require __DIR__ . '/../vendor/autoload.php';
+$autoloadFiles = [
+    __DIR__ . '/../../../autoload.php',
+    __DIR__ . '/../vendor/autoload.php',
+];
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        require_once $autoloadFile;
+        break;
+    }
+}
 
 $config = require __DIR__ . '/config.php';
 $container = new ServiceManager($config['dependencies']);
