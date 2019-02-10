3f5ce130c61f01f00107ee96d6ba9cca76e5b0fd
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-02-10 21:38:31 +0100

    Moved some array manipulating logic from CustomizableAppConfig to PathCollection

--- src/Model/CustomizableAppConfig.php
+++ src/Model/CustomizableAppConfig.php
@@ -9,11 +9,7 @@ use Shlinkio\Shlink\Installer\Config\Plugin\LanguageConfigCustomizer;
 use Shlinkio\Shlink\Installer\Config\Plugin\UrlShortenerConfigCustomizer;
 use Shlinkio\Shlink\Installer\Util\PathCollection;
 use Zend\Stdlib\ArraySerializableInterface;
-use Zend\Stdlib\ArrayUtils;
 use function array_key_exists;
-use function array_reduce;
-use function array_shift;
-use function count;
 
 final class CustomizableAppConfig implements ArraySerializableInterface
 {
@@ -168,54 +164,40 @@ final class CustomizableAppConfig implements ArraySerializableInterface
 
     public function getArrayCopy(): array
     {
-        $app = $this->mapExistingKeysToPaths(self::APP_CONFIG_MAP, $this->app);
-        $db = $this->buildConnectionConfig();
-        $translator = $this->mapExistingKeysToPaths(self::LANG_CONFIG_MAP, $this->language);
-        $urlShortener = $this->mapExistingKeysToPaths(self::URL_SHORTENER_CONFIG_MAP, $this->urlShortener);
+        $pathCollection = new PathCollection();
 
-        return array_reduce([$app, $db, $translator, $urlShortener], [ArrayUtils::class, 'merge'], []);
+        $this->mapExistingKeysToPaths(self::APP_CONFIG_MAP, $this->app, $pathCollection);
+        $this->buildConnectionConfig($pathCollection);
+        $this->mapExistingKeysToPaths(self::LANG_CONFIG_MAP, $this->language, $pathCollection);
+        $this->mapExistingKeysToPaths(self::URL_SHORTENER_CONFIG_MAP, $this->urlShortener, $pathCollection);
+
+        return $pathCollection->toArray();
     }
 
-    private function buildConnectionConfig(): array
+    private function buildConnectionConfig(PathCollection $pathCollection): void
     {
         $dbDriver = $this->database[DatabaseConfigCustomizer::DRIVER] ?? '';
-        $db = $this->mapExistingKeysToPaths(self::DB_CONFIG_MAP, $this->database);
+        $this->mapExistingKeysToPaths(self::DB_CONFIG_MAP, $this->database, $pathCollection);
 
         // Build dynamic database config based on selected driver
         if ($dbDriver === 'pdo_sqlite') {
-            $db['entity_manager']['connection']['path'] = self::SQLITE_DB_PATH;
+            $pathCollection->setValueInPath(self::SQLITE_DB_PATH, ['entity_manager', 'connection', 'path']);
         } elseif ($dbDriver === 'pdo_mysql') {
-            $db['entity_manager']['connection']['driverOptions'] = [
+            $pathCollection->setValueInPath([
                 // PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
                 1002 => 'SET NAMES utf8',
-            ];
+            ], ['entity_manager', 'connection', 'driverOptions']);
         }
-
-        return $db;
     }
 
-    private function mapExistingKeysToPaths(array $map, array $sourceConfig): array
+    private function mapExistingKeysToPaths(array $map, array $sourceConfig, PathCollection $pathCollection): void
     {
-        $result = [];
-
         foreach ($map as $key => $path) {
             if (! array_key_exists($key, $sourceConfig)) {
                 continue;
             }
 
-            $value = $sourceConfig[$key];
-            $ref =& $result;
-            while (count($path) > 1) {
-                $currentKey = array_shift($path);
-                if (! array_key_exists($currentKey, $ref)) {
-                    $ref[$currentKey] = [];
-                }
-
-                $ref =& $ref[$currentKey];
-            }
-            $ref[array_shift($path)] = $value;
+            $pathCollection->setValueInPath($sourceConfig[$key], $path);
         }
-
-        return $result;
     }
 }
