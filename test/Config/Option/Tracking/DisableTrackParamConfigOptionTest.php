db413f35e6a0632df53c4789c9ff0c4567314b4c
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-05-16 12:18:51 +0200

    Added test for deprecated config paths

--- test/Config/Option/Tracking/DisableTrackParamConfigOptionTest.php
+++ test/Config/Option/Tracking/DisableTrackParamConfigOptionTest.php
@@ -42,4 +42,32 @@ class DisableTrackParamConfigOptionTest extends TestCase
         self::assertEquals($expectedAnswer, $answer);
         $ask->shouldHaveBeenCalledOnce();
     }
+
+    /**
+     * @test
+     * @dataProvider provideConfigWithDeprecations
+     */
+    public function shouldBeAskedTransparentlyMigratesFromDeprecatedPath(PathCollection $config, bool $expected): void
+    {
+        $result = $this->configOption->shouldBeAsked($config);
+
+        self::assertEquals($expected, $result);
+        self::assertFalse($config->pathExists(['app_options', 'disable_track_param']));
+        self::assertEquals(!$result, $config->pathExists(['tracking', 'disable_track_param']));
+    }
+
+    public function provideConfigWithDeprecations(): iterable
+    {
+        yield 'deprecated is set, new is not' => [new PathCollection([
+            'app_options' => [
+                'disable_track_param' => 'something',
+            ],
+        ]), false];
+        yield 'deprecated is not set, new is' => [new PathCollection([
+            'tracking' => [
+                'disable_track_param' => 'something',
+            ],
+        ]), false];
+        yield 'neither deprecated nor new are set' => [new PathCollection(), true];
+    }
 }
