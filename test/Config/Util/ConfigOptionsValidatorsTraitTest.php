145bf7b77a0204c25731977bc600ad325ee3c238
Alejandro Celaya <alejandro@alejandrocelaya.com>
2021-09-26 17:35:07 +0200

    Added method to validate a number is between two values

--- test/Config/Util/ConfigOptionsValidatorsTraitTest.php
+++ test/Config/Util/ConfigOptionsValidatorsTraitTest.php
@@ -91,9 +91,8 @@ class ConfigOptionsValidatorsTraitTest extends TestCase
     /**
      * @test
      * @dataProvider provideValidValues
-     * @param mixed $value
      */
-    public function validatePositiveNumberCastsToIntWhenProvidedValueIsValid($value, int $expected): void
+    public function validatePositiveNumberCastsToIntWhenProvidedValueIsValid(mixed $value, int $expected): void
     {
         self::assertEquals($expected, $this->validators->validatePositiveNumber($value));
     }
@@ -104,5 +103,55 @@ class ConfigOptionsValidatorsTraitTest extends TestCase
         yield 'positive as integer' => [5, 5];
         yield 'one as string' => ['1', 1];
         yield 'one as integer' => [1, 1];
+    }
+
+    /**
+     * @test
+     * @dataProvider provideInvalidNumbersBetween
+     */
+    public function validateNumberBetweenThrowsExceptionWhenProvidedValueIsInvalid(
+        mixed $value,
+        int $min,
+        int $max,
+    ): void {
+        $this->expectException(InvalidConfigOptionException::class);
+        $this->validators->validateNumberBetween($value, $min, $max);
+    }
+
+    public function provideInvalidNumbersBetween(): iterable
+    {
+        yield 'string' => ['foo', 1, 2];
+        yield 'lower as int' => [10, 20, 30];
+        yield 'lower as string' => ['10', 20, 30];
+        yield 'right before as int' => [19, 20, 30];
+        yield 'right before as string' => ['19', 20, 30];
+        yield 'right after as int' => [31, 20, 30];
+        yield 'right after as string' => ['31', 20, 30];
+        yield 'greater as int' => [50, 20, 30];
+        yield 'greater as string' => ['300', 20, 30];
+        yield 'impossible range' => [15, 30, 20];
+    }
+
+    /**
+     * @test
+     * @dataProvider provideValidNumbersBetween
+     */
+    public function validateNumberBetweenCastsToIntWhenProvidedValueIsValid(
+        mixed $value,
+        int $min,
+        int $max,
+        int $expected
+    ): void {
+        self::assertEquals($expected, $this->validators->validateNumberBetween($value, $min, $max));
+    }
+
+    public function provideValidNumbersBetween(): iterable
+    {
+        yield 'first as string' => ['20', 20, 30, 20];
+        yield 'first as int' => [20, 20, 30, 20];
+        yield 'between as string' => ['30', 20, 40, 30];
+        yield 'between as int' => [25, 20, 40, 25];
+        yield 'last as string' => ['55', 20, 55, 55];
+        yield 'last as int' => [55, 20, 55, 55];
     }
 }
