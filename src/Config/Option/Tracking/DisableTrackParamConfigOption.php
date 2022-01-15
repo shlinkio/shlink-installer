76e2af523e73e8c49960d7ddf818c6e7d9c62c26
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-01-15 10:29:16 +0100

    Fixed tracking config options not being properly dependent on global tracking

--- src/Config/Option/Tracking/DisableTrackParamConfigOption.php
+++ src/Config/Option/Tracking/DisableTrackParamConfigOption.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace Shlinkio\Shlink\Installer\Config\Option\Tracking;
 
 use Shlinkio\Shlink\Config\Collection\PathCollection;
-use Shlinkio\Shlink\Installer\Config\Option\BaseConfigOption;
 use Symfony\Component\Console\Style\StyleInterface;
 
-class DisableTrackParamConfigOption extends BaseConfigOption
+class DisableTrackParamConfigOption extends AbstractDisableTrackingDependentConfigOption
 {
     public function getEnvVar(): string
     {
--- src/Config/Option/Tracking/DisableTrackingFromConfigOption.php
+++ src/Config/Option/Tracking/DisableTrackingFromConfigOption.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace Shlinkio\Shlink\Installer\Config\Option\Tracking;
 
 use Shlinkio\Shlink\Config\Collection\PathCollection;
-use Shlinkio\Shlink\Installer\Config\Option\BaseConfigOption;
 use Symfony\Component\Console\Style\StyleInterface;
 
-class DisableTrackingFromConfigOption extends BaseConfigOption
+class DisableTrackingFromConfigOption extends AbstractDisableTrackingDependentConfigOption
 {
     public function getDeprecatedPath(): array
     {
