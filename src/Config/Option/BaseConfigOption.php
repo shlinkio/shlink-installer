f648abb58bc7f945dd114b651039f231da04ff72
Alejandro Celaya <alejandrocelaya@gmail.com>
2022-07-29 11:25:37 +0200

    Fixed BaseConfigOption check of an existing key

--- src/Config/Option/BaseConfigOption.php
+++ src/Config/Option/BaseConfigOption.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Installer\Config\Option;
 
+use function array_key_exists;
+
 abstract class BaseConfigOption implements ConfigOptionInterface
 {
     public function shouldBeAsked(array $currentOptions): bool
     {
-        return ! isset($currentOptions[$this->getEnvVar()]);
+        return ! array_key_exists($this->getEnvVar(), $currentOptions);
     }
 }
