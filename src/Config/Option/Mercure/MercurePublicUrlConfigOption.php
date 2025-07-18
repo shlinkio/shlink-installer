6cad0a93973cdb5772e180bdc47e50def9c34a05
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-07-18 08:41:36 +0200

    Replace AskUtilsTrait with ConfigOptionsValidator::validateRequired()

--- src/Config/Option/Mercure/MercurePublicUrlConfigOption.php
+++ src/Config/Option/Mercure/MercurePublicUrlConfigOption.php
@@ -4,13 +4,11 @@ declare(strict_types=1);
 
 namespace Shlinkio\Shlink\Installer\Config\Option\Mercure;
 
-use Shlinkio\Shlink\Installer\Util\AskUtilsTrait;
+use Shlinkio\Shlink\Installer\Config\Util\ConfigOptionsValidator;
 use Symfony\Component\Console\Style\StyleInterface;
 
 class MercurePublicUrlConfigOption extends AbstractMercureEnabledConfigOption
 {
-    use AskUtilsTrait;
-
     public function getEnvVar(): string
     {
         return 'MERCURE_PUBLIC_HUB_URL';
@@ -18,6 +16,9 @@ class MercurePublicUrlConfigOption extends AbstractMercureEnabledConfigOption
 
     public function ask(StyleInterface $io, array $currentOptions): string
     {
-        return $this->askRequired($io, 'public hub URL', 'Public URL of the mercure hub server');
+        return $io->ask(
+            'Public URL of the mercure hub server',
+            validator: static fn ($value) => ConfigOptionsValidator::validateRequired($value, 'public hub URL'),
+        );
     }
 }
